c9805ed73e6bcab0b7e8a75411d557178539cb7c
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-11 16:12:03 +0100

    Print stats on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @days = Day.all_cached    
-    # @stats = Rails.cache.stats.first.last
+    @days = Day.all_cached
+    @stats = Rails.cache.stats.first.last
   end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,4 @@
 class Day < ActiveRecord::Base
-
   def self.all_cached
      Rails.cache.fetch('Day.all') { all }
   end
