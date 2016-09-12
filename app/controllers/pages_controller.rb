11a9f7077fbef2f0742908bd307acf15d5de9e39
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-12 23:37:47 +0100

    Add our house page and fix multiline headers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,10 @@ class PagesController < ApplicationController
   end
 
   def philosophy
-    @page_heading = "Nobody cares"
+    @page_heading = "Nobody<br />Cares"
+  end
+
+  def house
+    @page_heading = "Our<br />House"
   end
 end
