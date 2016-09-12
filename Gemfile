87586bddd54709d76cf0fd1525485cd81ed8b8c6
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-12 20:09:34 +0100

    Notify slack when a new thought is created

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'thin'
 gem 'dalli'
 gem 'memcachier'
 
+gem 'httpclient'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,2 +1,7 @@
 class Thought < ActiveRecord::Base
+  after_create :notify_slack
+
+  def notify_slack
+    HTTPClient.post("https://hooks.slack.com/services/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", { payload: {username: thinker || "Randy Rando",  text: body, icon_emoji: ":intriguing:"}.to_json })
+  end
 end
