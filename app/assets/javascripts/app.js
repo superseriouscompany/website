09f998a2254fe7e25119ff14a790f18746abf17f
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-12 21:07:17 +0100

    Style idea submission form

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,10 @@ $(document).on('turbolinks:load', function() {
   ga('send', 'pageview');
 });
 
+$(document).on('keyup', '.js-detect-filled', function() {
+  var input = $(this);
+  input.parents('form').toggleClass('submittable', !!input.val());
+})
 
 $(document).on('click', '.js-hamburger', function() {
   $(document.body).toggleClass('hamburgerOpen');
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -2,6 +2,12 @@ class Thought < ActiveRecord::Base
   after_create :notify_slack
 
   def notify_slack
-    HTTPClient.post("https://hooks.slack.com/services/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", { payload: {username: thinker || "Randy Rando",  text: body, icon_emoji: ":intriguing:"}.to_json })
+    payload = {
+      username: thinker || "Randy Rando",
+      text: body,
+      icon_emoji: ":intriguing:"
+    }
+    payload[:channel] = "@neil" if Rails.env.development?
+    HTTPClient.post("https://hooks.slack.com/services/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", { payload: payload.to_json})
   end
 end
