052803357c59d53385098d769289e2a53e8e2d54
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-12 21:52:41 +0100

    Checkpoint: fix up small header menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     @day = Day.last
   end
+
+  def ideas
+    @page_heading = "Ideas"
+  end
+
+  def philosophy
+    @page_heading = "Nobody cares"
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find_by_slug! params[:slug]
+    @page_heading = "Projects"
   end
 end
