49e518a138b6b333c1c774f7bae58e1146492edf
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-12 23:07:07 +0100

    Send all traffic to /projects to the project page for this website

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   post '/thoughts' => 'thoughts#create'
+  get '/projects', to: redirect('/projects/this-website')
   get '/projects/:slug' => 'projects#show'
   get '/:action', controller: 'pages'
 end
