8d37cf632a1e0525f1170ff3a290cf26bb6e6ab8
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-18 14:32:15 +0100

    Fix share url on day page

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,6 +1,7 @@
 class DaysController < ApplicationController
   def show
     @day = Day.find_by_id(params[:id])
+    @share_image_url = "http://superseriouscompany.com#{ActionController::Base.helpers.asset_url("comic#{params[:id]}.jpg")}"
     redirect_to '/' unless @day.present?
   end
 end
