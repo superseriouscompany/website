bc1cf95c5d88760f1311fb716abcc15b9d6fbd67
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-21 15:04:19 +0100

    Checkpoint: add stub route for days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,13 @@
 class DaysController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def show
     @day = Day.find_by_id(params[:id])
     @share_image_url = "http://superseriouscompany.com#{ActionController::Base.helpers.asset_url("comic#{params[:id]}.jpg")}"
     redirect_to '/' unless @day.present?
+  end
+
+  def create
+    head 204
   end
 end
