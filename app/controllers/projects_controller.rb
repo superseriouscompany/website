91b962511cdd7da8dce08dd94505ca6e4bed748d
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-09-29 12:52:30 +0100

    Set order explicitly to avoid out of order days

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find_by_slug! params[:slug]
+    @days = @project.days.order("id asc")
     @page_heading = @project.name
   end
 end
