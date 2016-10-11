97a8f2037be54a3f9e14d9c0aaf2e52b93e0d1b6
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-11 22:35:39 +0100

    Add notify method on single subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -15,4 +15,18 @@ class Subscription < ActiveRecord::Base
 
     [response.status, response.body]
   end
+
+  def notify
+    payload = {
+      registration_ids: [fcm_id]
+    }
+    headers = {
+      "Authorization" => 'key=AIzaSyC1HZtvMnW0ft7wrYqlvb61QFO6hXNQWZU',
+      "Content-Type" => 'application/json'
+    }
+
+    response = HTTPClient.post("https://android.googleapis.com/gcm/send", payload.to_json, headers)
+
+    [response.status, response.body]
+  end
 end
