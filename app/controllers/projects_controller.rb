6bd485aafb9c5a32844caee7cc7e8ac44b7e8d62
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-11 23:26:10 +0100

    Reverse chron on projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   def show
     @project          = Project.find_by_slug! params[:slug]
-    @days             = @project.days.order("id asc")
+    @progress_updates = @project.progress_updates.order("id desc")
     @page_heading     = @project.name
     @page_title       = @project.name
     @meta_description = "#{@project.name}: a Super Serious Company effort"
