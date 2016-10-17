0a4172b0cf388018d5f731e03312c1297bccc8c6
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-17 22:34:38 +0100

    Remove ideas submission form

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,27 +8,6 @@
     }
   });
 
-  // Ideas form submission
-  $(document).on('submit', '.js-submission-form', function() {
-    var form = $(this);
-    var $body = form.find('.js-body');
-    var $thinker = form.find('.js-thinker');
-
-    if( !$body.val() ) {
-      return false;
-    }
-
-    if( $body.is(':visible') ) {
-      $body.hide();
-      $thinker.show().focus();
-      return false;
-    }
-  })
-  $(document).on('keyup', '.js-detect-filled', function() {
-    var input = $(this);
-    input.parents('form').toggleClass('submittable', !!input.val());
-  })
-
   // Hamburger menu behavior
   $(document).on('click', '.js-hamburger', function() {
     $(document.body).toggleClass('hamburgerOpen');
