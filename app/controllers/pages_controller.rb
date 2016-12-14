a5b00e0919b0221f12b801456c18cb77b3b849e8
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-14 14:51:00 +0000

    Fix share metadata on founder pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,7 +28,7 @@ class PagesController < ApplicationController
     redirect_to '/' and return unless @day.present?
     @page_title = "#{@day.portuguese_title} Cartoon"
     @meta_description = "Cartoon of day #{@day.id} in Portugal for Neil and Santi of Super Serious Company"
-    @share_image_url = @day.image_url.match(/\.com/) ? @day.image_url : "http://superseriouscompany.com#{ActionController::Base.helpers.asset_url("comic#{params[:id]}.jpg")}"
+    @share_image_url = @day.image_url.match(/\.com/) ? @day.image_url : "https://superseriouscompany.com#{ActionController::Base.helpers.asset_url("comic#{params[:id]}.jpg")}"
   end
 
   def show_project
@@ -51,18 +51,24 @@ class PagesController < ApplicationController
   def neil
     @page_title = "Neil Sarkar"
     @person = 'neil'
+    @meta_description = "Neil Rajat Sarkar is the entire Super Serious Company engineering department."
+    @share_image_url = "https://superseriouscompany.com#{ActionController::Base.helpers.asset_url("neil-sarkar/neil-ashley-jeremy-rooftop-arts-district.jpg")}"
     render :founder
   end
 
   def santi
     @page_title = "Santi Garza"
     @person = 'santi'
+    @meta_description = "Santi owns the visual brand of Super Serious Company and draws all the cartoons. He's active as fuck and has a magnetic personality. His outgoing and earnest exterior belies an existential turmoil and an artistic struggle to represent the beauty he perceives around him."
+    @share_image_url = "https://superseriouscompany.com#{ActionController::Base.helpers.asset_url("super-serious-santi-santiago-garza/santiago-garza-santi-tinder-desk-working-los-angeles-california.jpg")}"
     render :founder
   end
 
   def andrew
     @page_title = "Andrew Sauer"
     @person = 'andrew'
+    @meta_description = "Andrew Sauer is three children in a trenchcoat pretending to be an adult man."
+    @share_image_url = "https://superseriouscompany.com#{ActionController::Base.helpers.asset_url("andrew-sauer/andrew-sauer-animal-menagerie.jpg")}"
     render :founder
   end
 end
