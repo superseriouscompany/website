954e03b80f9f63b3f3b825b0a0aad9e76d85d803
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-15 14:53:40 +0000

    Fix progress update browser

--- app/assets/javascripts/projectBrowser.js
+++ app/assets/javascripts/projectBrowser.js
@@ -1,16 +1,31 @@
 (function() {
-  $(document).on('click', '.js-toggle-lightbox', function() {
-    var ret = $(document.body).toggleClass('lightbox-open');
+  var currentIndex = null;
 
+  $(document).on('click', '.js-toggle-lightbox', function() {
+    $(document.body).toggleClass('lightbox-open');
+  })
+
+  $(document).on('click', '.js-load-project-update', function() {
+    var id = $(this).data('update-id');
+    if( id === undefined ) { return console.error("No update id found on", this); }
+    loadUpdate(id);
   })
 
   $(document).on('click', '.js-next-project-update', function() {
-    $('.js-stage').html($('#update-0').html());
-    console.log("next");
+    loadUpdate(currentIndex+1);
   })
 
   $(document).on('click', '.js-prev-project-update', function() {
-    $('.js-stage').html($('#update-1').html());
-    console.log("previous");
+    loadUpdate(currentIndex-1);
   })
+
+  function loadUpdate(id) {
+    if( id < 0 ) { return; }
+    var template = $('#update-'+id);
+    if( !template.length ) { return; }
+
+    $('.js-stage').html(template.html());
+    $('.js-current-slide').text(id+1);
+    currentIndex = id;
+  }
 })()
