3e41169f2f2b3a295c48121ea865f9ab0ef3937b
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-17 00:57:06 +0000

    Add thumbnail url to progress updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216193340) do
+ActiveRecord::Schema.define(version: 20161217005555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20161216193340) do
     t.text     "description"
     t.string   "image_url"
     t.string   "project_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "creator"
+    t.string   "thumbnail_url"
   end
 
   create_table "projects", force: :cascade do |t|
