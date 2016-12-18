690a3fe02783b72f279c57e95bde3ece15ab3109
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-18 02:44:09 +0000

    Allow creating progress update by slug

--- app/controllers/progress_updates_controller.rb
+++ app/controllers/progress_updates_controller.rb
@@ -2,7 +2,12 @@ class ProgressUpdatesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    progress_update = Project.order("id desc").first.progress_updates.new(progress_update_params)
+    if params[:project_slug].present?
+      progress_update = Project.find_by_slug!(params[:project_slug]).progress_updates.new(progress_update_params)
+    else
+      progress_update = Project.order("id desc").first.progress_updates.new(progress_update_params)
+    end
+
 
     if progress_update.save
       render status: 201, json: { id: progress_update.id }
