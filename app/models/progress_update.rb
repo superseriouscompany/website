00e314c94b79d0fe05f3de30eb6a0ca34b1f9fc3
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-18 04:07:22 +0000

    Add method to create thumbnail from progress update

--- app/models/progress_update.rb
+++ app/models/progress_update.rb
@@ -1,3 +1,12 @@
 class ProgressUpdate < ActiveRecord::Base
   belongs_to :project
+
+  def create_thumbnail
+    payload = { url: image_url }
+    headers = {
+      "Content-Type" => 'application/json'
+    }
+    resp = HTTPClient.post("https://90gkxhh9u7.execute-api.us-west-2.amazonaws.com/Production", payload.to_json, headers)
+    update_attribute :thumbnail_url, JSON.parse(resp.body)["url"]
+  end
 end
