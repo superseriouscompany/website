d6f9d6e9c6769c7a962810fe0ce2c4b23e45187d
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-05-11 16:38:44 +0100

    Set share metadata for ideas page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,14 @@
 class IdeasController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
+  def new
+    @meta_description = "The last hurrah."
+    @share_image_url = "https://tribkcpq.files.wordpress.com/2015/02/s042881738-300.jpg"
+  end
+
   def create
+    @meta_description = "The last hurrah."
+    @share_image_url = "https://tribkcpq.files.wordpress.com/2015/02/s042881738-300.jpg"
     @idea = Idea.new(idea_params)
     if @idea.save
       @cool = true
